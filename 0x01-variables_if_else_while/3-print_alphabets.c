994f17fa77f8b0c86f75c92c0d2676f81a6fa40d
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2023-06-16 17:54:53 -0700

    go

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int I;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (I = 'A' ; I <= 'Z' ; I++)
+		putchar(I);
+	return (0);
+}
