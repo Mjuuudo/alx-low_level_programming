aa63cbddae97d461a88cb6ff2bbad67b9793a4ea
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2023-06-19 13:40:36 -0700

    ready

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Return: 10 first Digits
+ *main - entry point
+ *Return: 10 first Digits
  */
-int main (void)
+int main(void)
 {
 	int nb = 48;
 	int i;
 
-	for(i = nb ; i <= nb+9; i++)
+	for (i = nb ; i <= nb + 9; i++)
 	{
 		putchar(i);
 	}
 	putchar('\n');
 	return (0);
-
 }
