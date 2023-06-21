f7ce03dc46e26e2684c41a47f7048e7f4d9c72ff
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2023-06-21 03:32:18 -0700

    ready

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include <main.h>
 /**
  *main - entry point
  *Return: 10 first Digits
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
