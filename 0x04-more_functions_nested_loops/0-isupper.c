ba616b61044889254e8201af467a7bcf0dbfaffd
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2023-06-23 16:36:32 -0700

    ready

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - i suppeur case or lower case
+ * @c: number entred
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
