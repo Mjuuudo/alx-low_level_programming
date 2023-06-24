1e865992e9e9c69a9608b4ff544201c2cd1131b0
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2023-06-23 18:37:34 -0700

    ready

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - more numbers to print
+ * Return: always 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+		{
+			if (j > 9)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
+
