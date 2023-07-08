cb9cdea27f25aefc81441be8bd597b722c4c970f
Mjuuudo <abdelmajid.aitouaziz@gmail.com>
2023-07-07 17:23:06 -0700

    ready

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - fn entry
+ * @argc: int
+ * @argv: liste
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - fn entry
+ * @argc: nb
+ * @argv: liste
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
